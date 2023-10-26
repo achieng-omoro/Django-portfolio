76e8359dd6f5cfd65bdd56623c3e1e6123cb04d2
achieng omoro <omoromichael@gmail.com>
2023-10-26 11:30:49 +0300

    django commit

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+from home import views
+
+app_name="home"
+
+urlpatterns = [
+
+    path('',views.home, name="homepage"),
+    path('services',views.services,name='servicespage'),
+    path('works', views.works, name='workspage'),
+    path('contact', views.contact, name='contactpage')
+]
