ffbb2b4c3d9bcd0c5fa40b4bcd0ac61d25c99ce4
achieng omoro <omoromichael@gmail.com>
2023-10-26 14:04:36 +0300

    added CRUD capability for add and view data

--- home/urls.py
+++ home/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('',views.home, name="homepage"),
     path('services',views.services,name='servicespage'),
     path('works', views.works, name='workspage'),
-    path('contact', views.contact, name='contactpage')
+    path('contact', views.contact, name='contactpage'),
+    path('add',views.add, name="addpage"),
+    path('view',views.view, name="viewdata")
+
 ]
--- home/views.py
+++ home/views.py
@@ -11,3 +11,9 @@ def works(request):
     return render(request,'home.html')
 def contact(request):
     return render(request,'home.html')
+
+
+def add(request):
+    return render(request,'adddata.html')
+def view(request):
+    return render(request,'viewdata.html')
